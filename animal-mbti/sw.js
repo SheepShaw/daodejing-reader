9665d65c5207341a6a2b8d2a1c05ecbb9383cf86
W <deploy@local>
2026-07-28 16:25:23 +0800

    add animal-mbti site (2026-07-28): PWA, from CloudStudio b9ed80a6

---
+++ animal-mbti/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'animal-mbti-v23';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        // Cache new same-origin GET requests for offline use
+        if (event.request.method === 'GET' && new URL(event.request.url).origin === self.location.origin) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
